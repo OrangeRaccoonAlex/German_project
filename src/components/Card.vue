406cdfab72f01ee8096a5cb7afa294aeb5002ed2
Шурочка Потапова <alex@MacBook-Pro-Surocka.local>
2023-10-05 23:26:35 +0300

    feat: накидала json, сделала первое подобие будущих карточек с вопросами

---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+<!--  TODO в слеующий раз вывести в виде отдельных карточек-->
+  <div v-for="card in answers.card" :key="card.id">
+    <div>
+      <!--TODO подумать, как вывести картинку-->
+      <img alt="ебаная картинка" style="max-height: 200px" :src="card.img">
+    </div>
+
+    <div>
+      <span class="text-black">{{ card.question }}</span>
+    </div>
+
+    <div class="bg-green-400">прогрессбар</div>
+
+    <div v-for="answer in card.answers" :key="answer.id" class="flex items-center">
+      <!-- TODO не работают стили на чек-бокс-->
+      <input
+          type="checkbox"
+          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+      />
+      <div class="pl-4">
+        <p class="text-xl font-medium text-black">{{ answer.answer }}</p>
+        <p class="text-green-700" v-if="answer.correct">верный ответ</p>
+        <p class="text-red-500" v-if="!answer.correct">не верный ответ</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import answers from '../../data/answers.json'
+// import {ref, onMounted, watch} from "vue";
+
+// const answerCardLocalState = ref('answerCard')
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/MainCardBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-white p-5 border-blue-400 border-2 rounded-md">
+    <Card />
+    <Actions />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import Actions from "../components/Actions.vue";
+import Card from "../components/Card.vue";
+</script>
+
+<style scoped>
+
+</style>
